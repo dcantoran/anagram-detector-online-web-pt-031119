e1a597b29199208e3b72cf7654608d42f31e9e05
David Cantoran <davidcantoran36@gmail.com>
2019-04-08 02:44:52 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,2 +1,16 @@
 # Your code goes here!
 
+class Anagram
+  attr_accessor :name
+  def initialize(name) 
+    @name = name
+  end 
+  
+  def match(words_arr) 
+    
+    words_arr.select do |idx|
+      @word.split("").sort == idx.split("").sort
+    
+  end 
+  
+end 
